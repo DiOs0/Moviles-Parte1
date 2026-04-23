1514335cfc60d00a971f5c6ea24d60a7e8583039
DiOs0 <dfosorioe@uce.edu.ec>
2026-04-22 19:06:05 -0500

    Introduccion a Kotlin Clase 22/04/2026

--- app/src/main/java/com/uce/moviles_parte1/LoginDeber.kt
+++ app/src/main/java/com/uce/moviles_parte1/LoginDeber.kt
@@ -1,20 +1,41 @@
 package com.uce.moviles_parte1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.uce.moviles_parte1.databinding.ActivityLoginDeberBinding
 
 class LoginDeber : AppCompatActivity() {
+
+    //Esto es para que cada objeto de esta clase que tenga el id se convierta en objeto directamente
+    lateinit var binding: ActivityLoginDeberBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login_deber)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityLoginDeberBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnSignUp.setOnClickListener {
+
+            var msg=""
+            if(binding.txtUsername.text.toString() == "Admin" && binding.txtPassword.text.toString() =="root") msg= "Usuario ingreso correctamente"
+            else msg="No pudo ingresar"
+
+            binding.btnSignUp.setOnClickListener {
+                Toast.makeText(this,msg,
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+
         }
     }
-}
